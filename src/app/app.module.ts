e5da27228386b8b9bf082063e6c3e6eca5723e34
merg13 <45442083+merg13@users.noreply.github.com>
2020-05-22 09:45:34 -0500

    big initial check-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardContainerComponent } from './card-container/card-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentCardComponent,
+    NavBarComponent,
+    FooterComponent,
+    CardContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/content-card/content-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent implements OnInit {
+
+  constructor() { }
+
+  // @Input() cardContents: any;
+  // @Input() title: String;
+  // @Input() tooltip: String;
+
+  ngOnInit() {
+  }
+
+}
